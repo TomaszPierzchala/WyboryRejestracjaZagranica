f4288b8b927c06da264a95916d21dc3c1d1ff763
TomaszPierzchala <t.pierzchala@gmail.com>
2024-05-25 13:21:50 +0200

    run plot.py added

--- run_update.sh
+++ run_update.sh
@@ -2,5 +2,6 @@
 
 cd /Users/tomek/git/WyboryRejestracjaZagranica/
 ./pickup_data.sh no_progress >> wyniki.out
+python3 plot.py
 
 #git add wyniki.out zestawienie.out; git commit -m "results update"; git push
