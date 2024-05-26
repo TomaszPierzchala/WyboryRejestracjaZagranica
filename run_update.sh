429dfe7b283e06c7b880ff2d29eb506cfb75507c
TomaszPierzchala <t.pierzchala@gmail.com>
2024-05-26 15:28:31 +0200

    updated

--- run_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /Users/tomek/git/WyboryRejestracjaZagranica/
-./pickup_data.sh no_progress >> wyniki.out
-python3 plot.py
-
-#git add wyniki.out zestawienie.out; git commit -m "results update"; git push
